5899d95a4de66774883bf3dc082ab6b9caee13c2
Andreas Blochberger <andreas@blochberger.net>
2025-01-13 17:26:41 +0100

    RIT-3 YouTrack import plugin

--- plugins/youtrack/src/importers/time.rs
+++ plugins/youtrack/src/importers/time.rs
@@ -1,6 +1,6 @@
 use config::{RiteYoutrackImportTime, TimeTracking};
 use import::Importer;
-use model::{field::Field, record::Record, value::Value, Initializable};
+use model::{field::Field, record::Record, Initializable};
 
 use crate::importers::connection::YouTrackConnection;
 
@@ -45,19 +45,29 @@ impl Initializable for YouTrackImporterTime {
 
 impl Importer for YouTrackImporterTime {
     fn read(&mut self, callback: import::RecordCallback) -> Result<(), Box<dyn std::error::Error>> {
+        if let Some(ref time_tracking) = self.time_tracking {
+            if let Some(ref connection) = self.connection {
+                if let Some(ref url) = connection.url {
+                    if let Some(ref token) = connection.token {
+                        make_request(callback, time_tracking, &url, &token)?;
+                    }
+                }
+            }
+        }
+
         let mut record = Record::new();
         if let Some(ref time_tracking) = self.time_tracking {
             if let Some(start_date) = time_tracking.start_date {
                 record.fields_as_mut().push(Field::new_value(
                     "start-date".to_string(),
-                    Value::Date(start_date),
+                    model::value::Value::Date(start_date),
                 ));
             }
 
             if let Some(end_date) = time_tracking.end_date {
                 record.fields_as_mut().push(Field::new_value(
                     "end-date".to_string(),
-                    Value::Date(end_date),
+                    model::value::Value::Date(end_date),
                 ));
             }
         }
@@ -69,4 +79,47 @@ impl Importer for YouTrackImporterTime {
     fn reset(&mut self) -> Result<(), Box<dyn std::error::Error>> {
         Ok(())
     }
+}
+
+/// Create a URL string from the dataset values
+pub fn create_url_from_dataset(_time_tracking: &TimeTracking, base_url: &str) -> String {
+    // format!(
+    //     "{}/api/workItems?fields=id,created,duration(minutes)),author(id,email)&startDate={}",
+    //     base_url, "2025-01-01",
+    // )
+    format!(
+        "{}/api/workItems?fields=id,created,duration(minutes),author(id,email)&startDate=2025-01-01",
+        base_url,
+    )
+}
+
+pub fn make_request(
+    callback: import::RecordCallback,
+    time_tracking_config: &TimeTracking,
+    base_url: &str,
+    token: &str,
+    // response_handler: ResponseHandler,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let client = reqwest::blocking::Client::new();
+    let url = create_url_from_dataset(&time_tracking_config, base_url);
+
+    let response = client.get(url).bearer_auth(token).send()?;
+    if !response.status().is_success() {
+        let body: serde_json::Value = response.json()?;
+        return Err(format!("{}: {}", body["error"], body["error_description"]).into());
+    } else {
+        // call the response handler
+        println!("{:#?}", response);
+
+        let mut record = Record::new();
+        if let Ok(text) = response.text() {
+            record
+                .fields_as_mut()
+                .push(Field::new_string("response".to_string(), text));
+        }
+
+        callback(&record);
+    }
+
+    Ok(())
 }
