0f2345daa0acbf053d7ad7a78ea29b1015fc45bc
Markus Heigl <markus.heigl@ciao-systems.com>
2025-03-04 16:07:22 +0100

    RIT-27 Need ARM64 Image

--- build-image.sh
+++ build-image.sh
@@ -2,9 +2,10 @@
 eval "$(ssh-agent -s)"
 ssh-add $1
 
-export DOCKER_BUILDKIT=1 
+export DOCKER_BUILDKIT=1
 docker buildx \
     build \
     --cache-from rite:builder \
     --ssh default=$SSH_AUTH_SOCK \
+    --platform linux/arm/v7,linux/arm64/v8,linux/amd64 \
     -t rite:latest .
