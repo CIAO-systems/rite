484a88f128cb58954d5919ee515c46c19b786584
Andreas Blochberger <andreas@blochberger.net>
2025-03-04 08:42:49 +0100

    RIT-26 Event signaling

--- plugins/common/src/template.rs
+++ plugins/common/src/template.rs
@@ -1,9 +1,11 @@
 use ::tera::{Context, Tera, Value};
 use export::Exporter;
 use model::Initializable;
+use std::error::Error;
 
 const CFG_TEMPLATE_FILE: &str = "templateFile";
 const CFG_OUTPUT_FILE: &str = "outputFile";
+const TEMPLATE_NAME: &str = "template";
 
 mod tera;
 
@@ -25,12 +27,12 @@ impl TemplateExporter {
         if let Some(ref template_file) = self.template_file {
             let mut tera = Tera::default();
             // Load a single template file
-            tera.add_template_file(template_file, Some("exporter"))?;
+            tera.add_template_file(template_file, Some(TEMPLATE_NAME))?;
 
             let mut context = Context::new();
             context.insert("records", &self.records);
 
-            match tera.render("exporter", &context) {
+            match tera.render(TEMPLATE_NAME, &context) {
                 Ok(rendered) => {
                     // write rendered string in output file
                     if let Some(ref output_file) = self.output_file {
@@ -38,8 +40,15 @@ impl TemplateExporter {
                     }
                 }
                 Err(e) => {
-                    log::error!("Error rendering {template_file}: {e} ");
-                    return Err(e.into());
+                    let error = if let Some(source) = e.source() {
+                        source
+                    } else {
+                        &e
+                    };
+
+                    let msg = format!("Error rendering {template_file}: {error}");
+                    log::error!("{msg}");
+                    return Err(msg.into());
                 }
             }
         }
@@ -81,8 +90,12 @@ impl Exporter for TemplateExporter {
 
 #[cfg(test)]
 mod tests {
-    use model::{field::add_field, record::Record};
+    use export::{Exporter, Signal};
+    use model::{field::add_field, record::Record, xml::config::Configuration, Initializable};
     use tera::{Context, Tera};
+    use uuid::Uuid;
+
+    use super::{TemplateExporter, CFG_OUTPUT_FILE, CFG_TEMPLATE_FILE};
 
     #[test]
     fn test_tera() -> Result<(), model::BoxedError> {
@@ -136,6 +149,36 @@ Record: {{ record.recordField.fieldName }}
             "\nLoop:\n1.\nString: field-value\nInt: 73\nRecord: sub-record-value\n\n",
             s
         );
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_exporter() -> Result<(), model::BoxedError> {
+        let mut exporter = TemplateExporter::new();
+        let mut config = Configuration::new();
+        config.insert_str(
+            CFG_TEMPLATE_FILE,
+            "../../data/common/templates/test_template.txt",
+        );
+        const TMP_FILE: &str = "/tmp/template_output.txt";
+        config.insert_str(CFG_OUTPUT_FILE, TMP_FILE);
+
+        exporter.init(Some(config))?;
+        exporter.event(Signal::Start)?;
+
+        let mut record = Record::new();
+        let uuid = Uuid::new_v4();
+        add_field(
+            record.fields_as_mut(),
+            "id",
+            model::value::Value::String(uuid.to_string()),
+        );
+        exporter.write(&record)?;
+        exporter.event(Signal::End)?;
+
+        let content = std::fs::read_to_string(TMP_FILE)?;
+        assert_eq!(format!("1. {}", uuid.to_string()), content);
 
         Ok(())
     }
