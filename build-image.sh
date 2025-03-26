3a1af56c83cf72bf8159418f1a3fa22374c3ace6
Andreas Blochberger <andreas@blochberger.net>
2025-03-26 10:27:44 +0100

    RIT-31 Add X-Personio attributes for requests

--- build-image.sh
+++ build-image.sh
@@ -7,5 +7,6 @@ docker buildx \
     build \
     --cache-from rite:builder \
     --ssh default=$SSH_AUTH_SOCK \
-    --platform linux/arm64/v8,linux/amd64 \ # build for both ARM and x86 https://docs.docker.com/build/building/multi-platform/
     -t rite:latest .
+
+#    --platform linux/arm64/v8,linux/amd64 \ # build for both ARM and x86 https://docs.docker.com/build/building/multi-platform/
