165787a20b3cdc28e696fb7bf17b20a996226c8d
Andreas Blochberger <andreas@blochberger.net>
2025-04-24 13:29:57 +0200

    RIT-35 Split up container image

--- base/rite/src/tests/manual.rs
+++ base/rite/src/tests/manual.rs
@@ -3,14 +3,11 @@ use model::xml;
 use plugin::Plugin;
 
 static PLUGIN_PATH: &str = if cfg!(debug_assertions) {
-    "../../target/debug"
+    "../target/debug"
 } else {
-    "../../target/release"
+    "../target/release"
 };
-static IMPORT_PLUGIN_NAME: &str = "example_import";
-
-static IMPORTER_NAME: &str = "text";
-
+static IMPORT_PLUGIN_NAME: &str = "rite_csv";
 static TEST_DATA: &str = "../../data/test/testfile.txt";
 
 fn load_importer() -> Result<Plugin, Box<dyn std::error::Error>> {
@@ -19,13 +16,12 @@ fn load_importer() -> Result<Plugin, Box<dyn std::error::Error>> {
 
 pub fn create_importer<'a>(
     importer_plugin: &'a mut Plugin,
-    importer_name: &str,
 ) -> Result<Box<dyn Importer>, Box<dyn std::error::Error>> {
     // 1. load plugin
     // 2. Call creator function in plugin for an importer
     // 3. Initialize the importer
     // 3. box it and return it
-    match importer_plugin.create_importer(Some(importer_name)) {
+    match importer_plugin.create_importer(None) {
         Ok(mut importer) => {
             let config = create_test_importer_config();
             let _ = importer.init(Some(config))?;
@@ -46,7 +42,7 @@ fn test_importer() -> Result<(), Box<dyn std::error::Error>> {
     helper::pwd();
 
     let mut importer_plugin = load_importer()?;
-    let mut importer = create_importer(&mut importer_plugin, IMPORTER_NAME)?;
+    let mut importer = create_importer(&mut importer_plugin)?;
 
     let config = create_test_importer_config();
     let _ = importer.init(Some(config))?;
@@ -54,12 +50,3 @@ fn test_importer() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-#[test]
-fn test_importer_no_config() -> Result<(), Box<dyn std::error::Error>> {
-    let mut importer_plugin = load_importer()?;
-    let mut importer = create_importer(&mut importer_plugin, IMPORTER_NAME)?;
-
-    // this importer *needs* a configuration
-    assert!(importer.init(None).is_err());
-    Ok(())
-}
