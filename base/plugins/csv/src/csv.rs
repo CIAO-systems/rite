bf06a16b601383222316cc99f61fd7e01aa1f4b1
Andreas Blochberger <andreas@blochberger.net>
2025-05-23 15:45:26 +0200

    RIT-48 [CSV] Support delimiter configuration

--- base/plugins/csv/src/csv.rs
+++ base/plugins/csv/src/csv.rs
@@ -2,9 +2,12 @@ use model::Initializable;
 
 const CFG_FILENAME: &str = "filename";
 const CFG_EXPORT_OVERWRITE: &str = "overwrite";
+const CFG_DELIMITER: &str = "delimiter";
 
+#[derive(Debug)]
 pub struct CSV {
     filename: Option<String>,
+    delimiter: Option<u8>,
     export_header_written: bool,
     export_override: bool,
 }
@@ -13,6 +16,7 @@ impl CSV {
     pub(crate) fn new() -> Self {
         CSV {
             filename: None,
+            delimiter: None,
             export_header_written: false,
             export_override: false,
         }
@@ -30,6 +34,10 @@ impl Initializable for CSV {
                 Some(value) => value.parse::<bool>()?,
                 None => false,
             };
+            self.delimiter = config
+                .get(CFG_DELIMITER)
+                .and_then(|s| s.as_bytes().first().copied());
+            log::debug!("CSV: {:?}", self);
         }
 
         Ok(())
