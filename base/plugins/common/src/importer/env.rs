c640cabf1961f18041fb98ad55d533bd4fe69e7f
Andreas Blochberger <andreas@blochberger.net>
2025-06-27 14:03:08 +0200

    RIT-55 Imports started with rite.sh do not use environment variables

--- base/plugins/common/src/importer/env.rs
+++ base/plugins/common/src/importer/env.rs
@@ -43,18 +43,19 @@ mod tests {
 
     #[test]
     fn test_importer() -> Result<(), BoxedError> {
+        // Arrange
         let mut importer = EnvImporter::new();
         let mut records = Vec::new();
         let mut handler = CollectingRecordHandler::new(&mut records);
 
-        //
         let expected_value = Uuid::new_v4();
         unsafe { std::env::set_var("TEST_VAR", expected_value.to_string()) };
 
+        // Act
         importer.read(&mut handler)?;
 
+        // Assert
         assert!(records.len() > 0);
-
         assert!(records.iter().any(|record| {
             if let Some(name_field) = record.field_by_name("name") {
                 if let Value::String(env_name) = name_field.value() {
@@ -70,6 +71,7 @@ mod tests {
             false
         }));
 
+        // Annihilate
         unsafe { std::env::remove_var("TEST_VAR") };
 
         Ok(())
