d079438622a5b3ac9717cceff4c40f6531ab8fd3
Andreas Blochberger <andreas@blochberger.net>
2025-09-18 13:52:46 +0200

    RIT-77 Increase coverage of extended plugins

--- extended/plugins/youtrack/src/importers/youtrack/factory.rs
+++ extended/plugins/youtrack/src/importers/youtrack/factory.rs
@@ -73,7 +73,7 @@ fn json2model(json_value: &serde_json::Value) -> Value {
             } else if let Some(n) = number.as_f64() {
                 Value::F64(n)
             } else {
-                Value::None
+                unreachable!();
             }
         }
         serde_json::Value::String(s) => Value::String(s.to_string()),
@@ -129,36 +129,4 @@ pub fn json_object_to_value_map(
 }
 
 #[cfg(test)]
-mod tests {
-    use model::value::Value;
-    use serde_json::json;
-
-    use super::{json2model, json_object_to_value_map};
-
-    static TEST_STR: &str = "This is not the droids, your're looking for";
-
-    #[test]
-    fn test_json2model() {
-        assert_eq!(Value::U64(42), json2model(&json!(42)));
-        assert_eq!(Value::I64(-42), json2model(&json!(-42)));
-        assert_eq!(Value::F64(42.73), json2model(&json!(42.73)));
-        assert_eq!(
-            Value::String(TEST_STR.to_string()),
-            json2model(&json!(TEST_STR))
-        );
-        assert_eq!(Value::None, json2model(&json!([1, 2, 3])));
-        assert_eq!(Value::None, json2model(&json!({"field": 42.73})));
-        assert_eq!(Value::Bool(false), json2model(&json!(false)));
-        assert_eq!(Value::Bool(true), json2model(&json!(true)));
-    }
-
-    #[test]
-    fn test_json_object_to_value_map() {
-        let map = json_object_to_value_map("prefix", &json!({"field": 42.73}));
-
-        assert_eq!(1, map.len());
-        let entry = map.iter().next().unwrap();
-        assert_eq!("prefix.field", entry.0);
-        assert_eq!(Value::F64(42.73), *entry.1);
-    }
-}
+mod tests;
