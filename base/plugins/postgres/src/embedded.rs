34e072598438ba4cde7278d389330219d5ae70f5
Andreas Blochberger <andreas@blochberger.net>
2025-09-24 09:29:06 +0200

    RIT-78 Increase coverage of custom plugins

--- base/plugins/postgres/src/embedded.rs
+++ base/plugins/postgres/src/embedded.rs
@@ -1,7 +1,8 @@
-use std::error::Error;
+use std::{env, error::Error};
 
 use postgres::{Client, NoTls};
 use postgresql_embedded::blocking::PostgreSQL;
+use uuid::Uuid;
 
 pub struct Embedded {
     pub postgresql: PostgreSQL,
@@ -13,6 +14,12 @@ impl Embedded {
         // Needs libxml2-legacy (arch) installed to work, or libxml2 (ubuntu?)
         let mut settings = PostgreSQL::default().settings().clone();
         settings.timeout = Some(std::time::Duration::from_secs(60));
+        let base = env::temp_dir()
+            .join("pg_embedded")
+            .join(Uuid::new_v4().to_string());
+        settings.data_dir = base.join("data");
+        settings.installation_dir = base.join("installation");
+
         let mut postgresql = PostgreSQL::new(settings);
 
         postgresql.setup()?;
