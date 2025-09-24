e03d90b39c6b5718b2e97a83717925eacbe041ec
Andreas Blochberger <andreas@blochberger.net>
2025-09-24 10:20:14 +0200

    RIT-78 Increase coverage of custom plugins

--- custom/plugins/ciao/src/connection/tests.rs
+++ custom/plugins/ciao/src/connection/tests.rs
@@ -16,6 +16,8 @@ use model::{
 };
 use tokio::runtime::Runtime;
 
+use crate::config::{CFG_API_KEY, CFG_URL};
+
 use super::CiaoConnection;
 
 static INIT: Once = Once::new();
@@ -187,4 +189,23 @@ fn manual_connection_projects() -> Result<(), BoxedError> {
     }
 
     Ok(())
+}
+
+#[test]
+fn test_connect_none() {
+    let connection = CiaoConnection::connect(&None);
+    assert!(connection.is_err_and(|e| e.to_string() == "Configuration incomplete"));
+}
+
+#[test]
+fn test_connect_some() {
+    let mut config = Configuration::new();
+    config.insert_str(CFG_URL, "incorrect://localhost:50051");
+    config.insert_str(CFG_API_KEY, "some-api-key");
+    let connection = CiaoConnection::connect(&Some(config));
+    println!("{:?}", connection);
+    assert!(connection.is_err_and(|e| {
+        println!("{:?}", e.to_string());
+        e.to_string() == "gRPC transport error: Failed to connect to gRPC server on incorrect://localhost:50051. Original error: transport error"
+    }));
 }
