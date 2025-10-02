f9a85dbf8dcda645053ac596a77c513ccb6e63d9
Andreas Blochberger <andreas@blochberger.net>
2025-10-02 14:25:14 +0200

    [postgres/import] Support for all datatypes #58

--- base/plugins/postgres/src/importer.rs
+++ base/plugins/postgres/src/importer.rs
@@ -80,12 +80,14 @@ impl Importer for PostgresImporter {
     }
 }
 
+/// convert a postgres::Row to a Record
+/// See PostgresSQL datatypes: https://www.postgresql.org/docs/current/datatype.html
 fn handle_row(row: postgres::Row) -> Result<Record, Box<dyn std::error::Error>> {
     let mut record = Record::new();
     for (idx, column) in row.columns().iter().enumerate() {
         let field_type = column.type_().name();
         match field_type {
-            "int4" => {
+            "integer" | "serial" | "int4" => {
                 let value: i32 = row.get(idx);
                 record
                     .fields_as_mut()
@@ -128,51 +130,4 @@ fn handle_row(row: postgres::Row) -> Result<Record, Box<dyn std::error::Error>> 
 }
 
 #[cfg(test)]
-mod tests {
-
-    use model::import::{handlers::ClosureRecordHandler, Importer};
-    use model::{xml, Initializable};
-
-    use super::PostgresImporter;
-
-    #[test]
-    #[ignore = "for manual testing"]
-    fn test_import() -> Result<(), Box<dyn std::error::Error>> {
-        let mut importer = PostgresImporter::new();
-        let config = xml::config::Configuration::with_xml("../../data/postgres-import-config.xml");
-
-        importer.init(Some(config))?;
-
-        let mut count = 0;
-        let mut handler = ClosureRecordHandler::new(|_record| {
-            count = count + 1;
-        });
-        importer.read(&mut handler)?;
-
-        assert!(count > 0);
-        Ok(())
-    }
-
-    #[test]
-    fn test_config() -> Result<(), Box<dyn std::error::Error>> {
-        let config = xml::config::Configuration::with_xml("../../data/postgres-import-config.xml");
-        let mut importer = PostgresImporter::new();
-        importer.init(Some(config))?;
-
-        assert!(importer.postgres.is_some());
-        let postgres = importer.postgres.unwrap();
-        assert_eq!(postgres.connection.host, "localhost".to_string());
-        assert_eq!(postgres.connection.port, 5432);
-        assert_eq!(postgres.connection.database, "postgres".to_string());
-        assert_eq!(postgres.connection.user, "postgres".to_string());
-        assert_eq!(
-            postgres.connection.password,
-            "6d598907-a775-4383-ab6f-de525c5ac0bf".to_string()
-        );
-
-        assert_eq!(postgres.sql, "select * from customers".to_string());
-        Ok(())
-    }
-
-    mod postgres;
-}
+mod tests;
--- base/plugins/postgres/src/importer/tests/postgres.rs
+++ base/plugins/postgres/src/importer/tests/postgres.rs
@@ -1,16 +1,17 @@
 use std::error::Error;
 
 use model::{
-    import::{handlers::ClosureRecordHandler, Importer},
+    Initializable,
+    import::{Importer, handlers::ClosureRecordHandler},
     value::Value,
-    xml, Initializable,
+    xml,
 };
 use postgres::Client;
 
 use crate::{
     common::Connection,
     embedded::Embedded,
-    importer::{config::RitePostgresImport, handle_row, PostgresImporter},
+    importer::{PostgresImporter, config::RitePostgresImport, handle_row},
 };
 
 #[test]
@@ -25,24 +26,58 @@ fn test_handle_row() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
+const CREATE_QUERY: &str =
+    r#"
+    CREATE TABLE 
+        dummy(
+            f1 int4, 
+            f2 int8, 
+            f3 text, 
+            f4 bool, 
+            f5 float4, 
+            f6 float8, 
+            f7 smallserial,
+            f8 int2,
+            f9 integer,
+            f100 serial,
+            f101 bigserial
+        );
+    "#;
+
+const INSERT_QUERY: &str = 
+    r#"
+    INSERT INTO dummy 
+            (f1,f2,f3,f4,f5,f6,f7,f8,f9) 
+        VALUES 
+            ($1,$2,$3,$4,$5,$6,$7,$8,$9) 
+        RETURNING 
+            f1,f2,f3,f4,f5,f6,f7,f8,f9,f100,f101
+    "#;
+
+const SELECT_QUERY: &str = 
+    r#"
+    SELECT 
+        f1,f2,f3,f4,f5,f6,f7,f8,f9,f100,f101
+    FROM dummy
+    "#;
+
 fn create_dummy_table(embeded: &mut Embedded) -> Result<(), Box<dyn Error>> {
     let mut transaction = embeded.client.transaction()?;
 
-    transaction.execute(
-        "CREATE TABLE dummy(f1 int4, f2 int8, f3 text, f4 bool, f5 float4, f6 float8, f7 smallserial);",
-        &[],
-    )?;
+    transaction.execute(CREATE_QUERY, &[])?;
     let params = (
-        73,
-        73 as i64,
-        "This are not the droids you are looking for",
-        true,
-        73.0 as f32,
-        73.0 as f64,
-        73 as i16,
+        73,                                             // f1
+        73 as i64,                                      // f2
+        "This are not the droids you are looking for",  // f3
+        true,                                           // f4
+        73.0 as f32,                                    // f5
+        73.0 as f64,                                    // f6
+        73 as i16,                                      // f7
+        42 as i16,                                      // f8
+        4273 as i32,                                    // f9
     );
-    let rec = transaction.query_one("INSERT INTO dummy (f1,f2,f3,f4,f5,f6,f7) VALUES ($1,$2,$3,$4,$5,$6,$7) RETURNING f1,f2,f3,f4,f5,f6,f7",
-        &[&params.0,&params.1,&params.2,&params.3,&params.4,&params.5,&params.6])?;
+    let rec = transaction.query_one(INSERT_QUERY,
+        &[&params.0,&params.1,&params.2,&params.3,&params.4,&params.5,&params.6,&params.7,&params.8])?;
     test_insert(rec);
 
     transaction.commit()?;
@@ -65,10 +100,18 @@ fn test_insert(rec: postgres::Row) {
     assert_eq!(value, 73.0);
     let value: i16 = rec.get("f7");
     assert_eq!(value, 73);
+    let value: i16 = rec.get("f8");
+    assert_eq!(value, 42);
+    let value: i32 = rec.get("f9");
+    assert_eq!(value, 4273);
+    let value: i32 = rec.get("f100");
+    assert_eq!(value, 1);
+    let value: i64 = rec.get("f101");
+    assert_eq!(value, 1);
 }
 
 fn test_supported(client: &mut Client) -> Result<model::record::Record, Box<dyn Error>> {
-    let recs = client.query("SELECT f1,f2,f3,f4,f5,f6 FROM dummy", &[])?;
+    let recs = client.query(SELECT_QUERY, &[])?;
     assert_eq!(recs.len(), 1);
     let row = recs.first();
     assert!(row.is_some());
@@ -80,12 +123,15 @@ fn test_supported(client: &mut Client) -> Result<model::record::Record, Box<dyn 
     assert!(f.is_some());
     let value = f.unwrap().value();
     assert!(matches!(value, Value::I32(73)));
+
     let f = record.field_by_name("f2");
     assert!(f.is_some());
     let value = f.unwrap().value();
     assert!(matches!(value, Value::I64(73)));
+
     let f = record.field_by_name("f3");
     assert!(f.is_some());
+
     let value = f.unwrap().value();
     assert!(matches!(value, Value::String(_)));
     assert_eq!(
@@ -96,14 +142,42 @@ fn test_supported(client: &mut Client) -> Result<model::record::Record, Box<dyn 
     assert!(f.is_some());
     let value = f.unwrap().value();
     assert!(matches!(value, Value::Bool(true)));
+
     let f = record.field_by_name("f5");
     assert!(f.is_some());
     let value = f.unwrap().value();
     assert!(matches!(value, Value::F32(73.0)));
+
     let f = record.field_by_name("f6");
     assert!(f.is_some());
     let value = f.unwrap().value();
     assert!(matches!(value, Value::F64(73.0)));
+
+    let f = record.field_by_name("f7");
+    assert!(f.is_some());
+    let value = f.unwrap().value();
+    assert!(matches!(value, Value::I8(1)));
+
+    let f = record.field_by_name("f8");
+    assert!(f.is_some());
+    let value = f.unwrap().value();
+    assert!(matches!(value, Value::I8(73)));
+
+    let f = record.field_by_name("f9");
+    assert!(f.is_some());
+    let value = f.unwrap().value();
+    assert!(matches!(value, Value::I32(4273)));
+
+    let f = record.field_by_name("f100");
+    assert!(f.is_some());
+    let value = f.unwrap().value();
+    assert!(matches!(value, Value::I32(1)));
+
+    let f = record.field_by_name("f101");
+    assert!(f.is_some());
+    let value = f.unwrap().value();
+    assert!(matches!(value, Value::I32(1)));
+
     Ok(record)
 }
 
